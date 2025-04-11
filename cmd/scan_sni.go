8ead5bab4ab79893f0e488384475d7d70fb45cb7
ayanrajpoot10 <ayanrajpoot2004@gmail.com>
2025-04-11 15:05:45 +0530

    fix: correct hardcoded IP address to use domain variable in scanSNI function

--- cmd/scan_sni.go
+++ cmd/scan_sni.go
@@ -50,13 +50,12 @@ func scanSNI(c *queuescanner.Ctx, p *queuescanner.QueueScannerScanParams) {
 		if dialCount > 3 {
 			return
 		}
-		conn, err = net.DialTimeout("tcp", "93.184.216.34:443", 3*time.Second)
+		conn, err = net.DialTimeout("tcp", domain+":443", 3*time.Second)
 		if err != nil {
 			if e, ok := err.(net.Error); ok && e.Timeout() {
 				c.LogReplace(p.Name, "-", "Dial Timeout")
 				continue
 			}
-			c.Logf("Dial error: %s", err.Error())
 			return
 		}
 		defer conn.Close()
